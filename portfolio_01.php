8f5d45766a94d2d151cd6045395a6487f533e068
jueun jin <74699797+pearlag@users.noreply.github.com>
2024-07-01 21:02:16 +0900

    Add files via upload

---
+++ portfolio_01.php
@@ -0,0 +1,410 @@
+<?php include_once('layout/header.html'); ?>
+
+  <section id="section">
+    <div id="CI_popup_modal">
+      <div id="CI_popup_wrap">
+        <div id="CI_popup">
+          <div class="subject">
+            <div>CI 소개</div>
+            <span class="xi-close-thin CI_popup_close"></span>
+          </div>
+          <a href="#none"><img src="./img/CI_popup.png" alt="CI 소개"></a>
+      </div>
+    </div>
+  </div>
+    <div id="proposal_fix">
+          <a href="./img/agency/jinjueun_proposal.pdf" class="popup" download="jinjueun_proposal.pdf">기획서<br>다운로드</a>
+        </div>
+    <h6>쉐어더로드</h6>
+    <section id="section1">
+      <h6>쉐어더로드 접이식헬멧</h6>
+      <div id="play_video">
+        <video src="./img/main_1920.mp4"
+        loop="loop" muted="muted" autoplay="autoplay">
+        </video>
+        <div class="main_textbox">
+          <p>
+            <span>Manners for each others,</span>
+            <span>safety for all of us.</span>
+            <span class="mainbanner_bar"></span>
+            <a href="/product/fend.html">자세히보기</a>
+          </p>
+        </div>
+      </div>
+    </section>
+    <section id="main_notice">
+      <h3 style="display: none;">공지사항</h3>
+      <div class="inner">
+        <div class="tit_b">
+          <span>NOTICE</span>
+          <strong>공지사항</strong>
+        </div>
+        <ul>
+          <li>
+            <a href="/community/notice_10.html">
+              <span>2020.12.06</span>
+              <strong>사회적 거리두기 2.5단계로 인한 라이딩 안전교육...</strong>
+            </a>
+          </li>
+          <li>
+            <a href="/community/notice_9.html">
+              <span>2020.09.24</span>
+              <strong>쉐어더로드, 헬멧 수퍼 워런티 프로그램 시행</strong>
+            </a>
+          </li>
+          <li>
+            <a href="/community/notice_8.html">
+              <span>2020.08.07</span>
+              <strong>니니 헬멧 구매 인증 이벤트 당첨자 발표</strong>
+            </a>
+          </li>
+          <li>
+            <a href="/community/notice_7.html">
+              <span>2020.07.13</span>
+              <strong>2020 쉐어더로드 X Rapha 라이프스타일 F/W시즌...</strong>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section id="section2">
+      <h3 style="visibility: hidden;">공지사항</h3>
+      <!--.main_con01>.bg_wrap(a>img)+.inner(>.left(>a>img)+.right(span+strong(>span)+p.mb(>span)+p+.more(>a)))-->
+      <div class="main_con01">
+        <div class="bg_wrap">
+          <a href="#none"><img src="./img/con01_bg.png" alt="사각형 두 개가 맞닿는 이미지"></a>
+        </div>
+        <div class="inner">
+          <div class="left">
+            <a href="/aboutus/technologies.html"><img src="./img/main_con01.png" alt="가방에서 헬멧을 꺼내는 사진"></a>
+          </div>
+          <div class="right">
+            <span>BUSINESS VALUE</span>
+            <strong>
+              쉐어더로드의 대표적인 기술<br>
+              <span>접이식 헬멧</span>에 대해 소개합니다.
+            </strong>
+            <p class="mb">
+              쉐어더로드 헬멧은 접었을 때 전체 크기의 60%까지 줄어드는
+              <br><span>폴딩 방식 헬멧</span>을 적용한 방식입니다. 접었을 때 백팩, 메신저백에<br>
+            쉽게 들어갈 수 있는 크기로, 도심 라이더에게 최적화된 디자인입니다.<br>또한 디자인 특허와 다양한 기술 특허를 갖고 있습니다.
+            </p>
+            <p>쉐어더로드 헬멧은 미국, 유럽, 한국 안전인증 시험을 모두 높은 점수로 통과하였습니다.<br>
+              접이식 헬멧은 접히는 부분이 충격에 약할 수가 있는데,<br>
+              많은 연구 끝에 접는 부분의 편의성과 강도를 모두 충족할 수 있었습니다.
+            </p>
+            <div class="more">
+              <a href="/aboutus/technologies.html">자세히보기</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+<!--section#section_03>.main_con02>.inner>.tit(>strong)+ul>li(>.img(>img)+.txt(>strong))*7-->
+    <section id="section_03">
+      <h3 style="visibility: hidden;">공지사항</h3>
+      <div class="main_con02">
+        <div class="inner">
+          <div class="tit">
+            <strong>파트너쉽</strong>
+          </div>
+          <ul>
+            <li>
+              <div class="img"><img src="./img/m_area01.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>서울시 공유자전거<br>'따릉이'<br>헬멧 납품<br><br></strong></div>
+            </li>
+            <li>
+              <div class="img"><img src="./img/m_area02.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>배달의민족<br>헬멧 납품</strong></div>
+            </li>
+            <li>
+              <div class="img"><img src="./img/m_area03.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>공유킥보드<br>'킥고잉'<br>헬멧 납품</strong></div>
+            </li>
+            <li>
+              <div class="img"><img src="./img/m_area04.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>인천시 공유자전거<br>'쿠키바이크'<br>헬멧 납품</strong></div>
+            </li>
+            <li>
+              <div class="img"><img src="./img/m_area05.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>대전시 공유자전거<br>'타슈'<br>헬멧 납품</strong></div>
+            </li>
+            <li>
+              <div class="img"><img src="./img/m_area06.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>서울시 대한자전거연맹<br>자전거 안전교육<br>헬멧 납품</strong></div>
+            </li>
+            <li>
+              <div class="img"><img src="./img/m_area07.png" alt="파트너쉽 업체"></div>
+              <div class="txt"><strong>인천시 교육청<br>학교로 찾아가는<br>자전거교육</strong></div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+<!-- section#section_04>.main_con03>.inner(>.main_tit(>strong+p+.more(>a[href="#"])))+.pro_list(>.inner(>ul(>li(>a(>.img_box(>img)+.text_box(>strong)))*3))+.people_wrap(>img+img))-->
+    <section id="section_04">
+      <h3 style="visibility: hidden;">공지사항</h3>
+      <div class="main_con03">
+        <div class="inner">
+          <div class="main_tit">
+            <strong>쉐어더로드 제품소개</strong>
+            <p>쉐어더로드의 대표적인 제품으로써 머리를 보호하고 사고시 두부손상을 방지하는<br>다양한 접이식 헬멧과 안전용품들을 소개합니다.</p>
+            <div class="more"><a href="/product/foldable_helmet.html">더보기</a></div>
+          </div>
+        </div>
+        <div class="pro_list">
+          <div class="inner" id="inner_in">
+            <ul class="pro_list_ul">
+              <li class="on">
+                <a href="/product/foldable_helmet.html">
+                  <div class="img_box">
+                    <img src="./img/product_on01.png" alt="접이식 헬멧">
+                  </div>
+                  <div class="text_box">
+                    <strong>접이식 헬멧</strong>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/safe_product.html">
+                  <div class="img_box">
+                    <img src="./img/product_on02.png" alt="안전용품">
+                  </div>
+                  <div class="text_box">
+                    <strong>안전용품</strong>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="/product/safe_product.html">
+                  <div class="img_box">
+                    <img src="./img/product_on03.png" alt="휴대용 공구">
+                  </div>
+                  <div class="text_box">
+                    <strong>휴대용 공구</strong>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="people_wrap">
+            <img src="./img/product_text.png" alt="perfect safety partners" class="con03_txt">
+            <img src="./img/main_bg0.png" alt="헬멧을 쓴 남성" class="people_img">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="section_05">
+      <h6 style="visibility: hidden;">공지사항</h6>
+      <div class="main_con04">
+      	<div class="inner">
+      		<div class="main_tit">
+      			<strong>베스트 상품</strong>
+      			<p>쉐어더로드의 다양한 헬멧 중에서 시즌을 가리지 않는 주문율과<br>
+      			만족도가 높은 베스트 상품들을 소개합니다.</p>
+      		</div>
+          <div class="case_slide">
+
+                <div class="wrap2 slick-slide" data-slick-index="0" aria-hidden="true" tabindex="-1" style="width: 994px;">
+                    <div>
+                        <div class="left_w">
+                        <a href="/product/caroline.html"><img src="./img/m_case01.png" alt="캐롤린 헬멧"></a>
+                        </div>
+                      <div class="right_w">
+                          <div class="text_box">
+                              <span>01</span>
+                              <strong>캐롤린 헬멧</strong>
+                              <p>누구에게나 잘 어울리는 디자인으로,<br>
+                              시즌 불문, 독보적인 인기 상품.</p>
+                          </div>
+                          <div class="next_w">
+                              <span>NEXT</span>
+                              <img src="./img/m_case02.png" alt="니니 헬멧">
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="wrap2 slick-slide" data-slick-index="0" aria-hidden="true" tabindex="-1" style="width: 994px;">
+                      <div>
+                          <div class="left_w">
+                          <a href="/product/nini.html"><img src="./img/m_case02.png" alt="니니 헬멧"></a>
+                          </div>
+                        <div class="right_w">
+                          <div class="text_box">
+                              <span>02</span>
+                              <strong>니니 헬멧</strong>
+                              <p>정장, 캐주얼, 원피스에도 어울리는<br>퓨어 화이트 헬멧.<br>
+                              여성분들에게 꾸준한 사랑을 받고 있는 상품.</p>
+                          </div>
+                          <div class="next_w">
+                              <span>NEXT</span>
+                              <img src="./img/m_case03.png" alt="빌라넬 헬멧">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                  <div class="wrap2 slick-slide" data-slick-index="0" aria-hidden="true" tabindex="-1" style="width: 994px;">
+                      <div>
+                          <div class="left_w">
+                          <a href="/product/vilanel.html"><img src="./img/m_case03.png" alt="빌라넬 헬멧"></a>
+                          </div>
+                        <div class="right_w">
+                          <div class="text_box">
+                              <span>03</span>
+                              <strong>빌라넬 헬멧</strong>
+                              <p>이런 컬러는 없었다.<br>
+                              포인트 주기 좋은 매력적인 보라색 헬멧.</p>
+                          </div>
+                          <div class="next_w">
+                              <span>NEXT</span>
+                              <img src="./img/m_case04.png" alt="엘레나 헬멧">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="wrap2 slick-slide" data-slick-index="0" aria-hidden="true" tabindex="-1" style="width: 994px;">
+                        <div>
+                            <div class="left_w">
+                            <a href="/product/elena.html"><img src="./img/m_case04.png" alt="엘레나 헬멧"></a>
+                            </div>
+                          <div class="right_w">
+                            <div class="text_box">
+                                <span>04</span>
+                                <strong>엘레나 헬멧</strong>
+                                <p>저렴한 가격과 코지한 디자인으로<br>
+                                젊은 고객층의 꾸준한 인기 상품.</p>
+                            </div>
+                            <div class="next_w">
+                                <span>NEXT</span>
+                                <img src="./img/m_case01.png" alt="캐롤린 헬멧">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+              </div>
+      		<ul>
+      			<li>
+      				<div class="wrap">
+
+      				</div>
+      			</li>
+      		</ul>
+      	</div>
+      </div>
+    </section>
+    <section id="section_06">
+      <h6 style="visibility: hidden;">공지사항</h6>
+      <div class="main_con05">
+      	<div class="inner">
+      		<div class="main_tit">
+      			<strong>진행중인 이벤트</strong>
+      			<p>쉐어더로드에서 진행중인 다양한 이벤트에 참여해보세요.</p>
+      		</div>
+      		<div class="cerf">
+            <div class="main_event">
+              <div class="main_event_01">
+                <div class="ab_box">
+                  <p class="event">
+                    <a href="#none">EVENT</a>
+                    <span>"블로그 체험단 모집"</span>
+                  </p>
+                  <a class="bar_text" href="/event/event_ing_1.html">자세히보기</a>
+                </div>
+              </div>
+              <div class="main_event_02">
+                <div class="ab_box">
+                  <p class="event">
+                    <a href="#none">EVENT</a>
+                    <span>"헬멧 시착 라이딩 이벤트"</span>
+                  </p>
+                  <a class="bar_text" href="/event/event_ing_2.html">자세히보기</a>
+                </div>
+              </div>
+              <div class="main_event_03">
+                <div class="ab_box">
+                  <p class="event">
+                    <a href="#none">EVENT</a>
+                    <span>"리뷰 이벤트"</span>
+                  </p>
+                  <a class="bar_text" href="/event/event_ing_3.html">자세히보기</a>
+                </div>
+              </div>
+              <div class="main_event_04">
+                <div class="ab_box">
+                  <p class="event">
+                    <a href="#none">EVENT</a>
+                    <span>"헬멧 인증샷 캠페인"</span>
+                  </p>
+                  <a class="bar_text" href="/event/event_ing_4.html">자세히보기</a>
+                </div>
+              </div>
+            </div>
+      		</div>
+      	</div>
+      </div>
+    </section>
+      <section id="section_07">
+        <div class="main_con06">
+          <div class="text_wrap">
+            <h6>쉐어더로드 베스트리뷰</h6>
+            <h3>베스트 리뷰</h3>
+            <p>쉐어더로드 고객님들의 베스트 리뷰입니다.</p>
+          </div>
+          <ul>
+            <li>
+              <a class="review" href="/community/review.html"><img src="./img/main_review_01.png" alt="#"></a>
+              <p>
+                <span class="reviewer_info">
+                  <span class="profile_01"></span>
+                  <span class="reviewer_id">gg2*** 님</span>
+                  <span class="reviewer_date">2020.12.03</span>
+                </span>
+                <span>
+                  아시안 핏이라 제 머리에 잘 맞아서<br>이거 따봉입니다.<br>
+다들 어디서 샀냐고 물어보고요<br>
+무게도 가벼운 편이라<br>출퇴근 용으로 잘 사용하고 있습니다<br>
+디자인이랑 휴대성까지 좋은데‥
+                </span>
+              </p>
+              <a class="detail" href="/community/review.html">자세히보기</a>
+            </li>
+            <li class="on">
+              <a class="review" href="/community/review.html"><img src="./img/main_review_02.png" alt="#"></a>
+              <p>
+                <span class="reviewer_info">
+                  <span class="profile_02"></span>
+                  <span class="reviewer_id">new1*** 님</span>
+                  <span class="reviewer_date">2020.11.19</span>
+                </span>
+                <span>
+                  1. 원하는 스타일의 헬멧!!<br>
+2. 접이식이라 공간 활용 백퍼!!<br>
+3. 남들이 안가지고 있는 스타일!!<br>
+그리고 가장 편했던 건 핏이 아시안핏이고,<br>
+턱끈조절이 가능해서 내 두상에 맞게<br>사이즈를 조절할 수 있었어요‥
+                </span>
+              </p>
+              <a class="detail" href="/community/review.html">자세히보기</a>
+            </li>
+            <li>
+              <a class="review" href="/community/review.html"><img src="./img/main_review_03.png" alt="#"></a>
+              <p>
+                <span class="reviewer_info">
+                  <span class="profile_03"></span>
+                  <span class="reviewer_id">suye0*** 님</span>
+                  <span class="reviewer_date">2021.12.29</span>
+                </span>
+                <span>정장을 입고 자출하는데,<br> 어울리는 헬멧을 찾아 고민하다가,<br>쉐어더로드를 알게 되었어요.<br>
+거래처 미팅하러 갈때<br> 헬멧이 너무 스포티하면 부담됐는데,<br>
+이 제품은 머리도 덜 눌리고,‥</span>
+              </p>
+              <a class="detail" href="/community/review.html">자세히보기</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+  </section>
+<?php include_once('layout/footer.html'); ?>
